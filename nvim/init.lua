5a0f752fed7c0bafe2ceb590ebca9e4b251421ec
Atishaya Maharjan <71453175+Atishaya7777@users.noreply.github.com>
2025-05-04 18:50:02 -0500

    feat: Added lldb and dap-ui

--- nvim/init.lua
+++ nvim/init.lua
@@ -202,7 +202,50 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  'github/copilot.vim',
+  -- 'github/copilot.vim',
+  'nvim-neotest/nvim-nio',
+  {
+    'rcarriga/nvim-dap-ui',
+    event = 'VeryLazy',
+    dependencies = 'mfussenegger/nvim-dap',
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+      dapui.setup()
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'mfussenegger/nvim-dap',
+    },
+    opts = {
+      handlers = {},
+    },
+  },
+  {
+    'mfussenegger/nvim-dap',
+    config = function(_, _)
+      -- PLUGIN: dapui keybindings
+      vim.keymap.set('n', '<leader>db', '<cmd> DapToggleBreakpoint <CR>', {
+        desc = 'Add breakpoint at line',
+      })
+      vim.keymap.set('n', '<leader>dr', '<cmd> DapContinue <CR>', {
+        desc = 'Start or continue the debugger',
+      })
+    end,
+  },
   {
     'norcalli/nvim-colorizer.lua',
     config = function()
@@ -607,6 +650,7 @@ require('lazy').setup({
             '--offset-encoding=utf-16',
           },
         },
+        codelldb = {},
         pylsp = {
           plugins = {
             pyflakes = { enabled = true },
