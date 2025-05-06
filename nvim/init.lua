60e1e59f511b4142d3849d720abf53b7488dc8e6
Atishaya Maharjan <71453175+Atishaya7777@users.noreply.github.com>
2025-05-06 16:34:30 -0500

    feat: Added nvim-surround

--- nvim/init.lua
+++ nvim/init.lua
@@ -205,6 +205,16 @@ require('lazy').setup({
   -- 'github/copilot.vim',
   'nvim-neotest/nvim-nio',
   {
+    'kylechui/nvim-surround',
+    version = '^3.0.0', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
     'rcarriga/nvim-dap-ui',
     event = 'VeryLazy',
     dependencies = 'mfussenegger/nvim-dap',
